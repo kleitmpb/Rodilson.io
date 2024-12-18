e9255b2d6922f219dd3e3eb116da83c427b204c1
kleitmpb <zeusr9251@gmail.com>
2024-12-18 11:08:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Desenvolvedor Web", "Programador", "Designer Gráfico"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
